500c1f01d96efc0ee4d3100e72f9c61530f93032
owenmoogk <owenmoogk@gmail.com>
2021-05-03 21:24:18 -0400

    added the nav

--- src/frontend/src/components/App.js
+++ src/frontend/src/components/App.js
@@ -1,10 +1,22 @@
-import React, {Component} from "react"
-import '../css/index.css'
-import Register from './Register.js'
+import React from "react"
+import '../index.css'
+import {
+	BrowserRouter as Router,
+	Route,
+	Switch,
+} from 'react-router-dom'
+import Register from './Register'
+import Nav from './nav/Nav'
 
 export default function App(){
 	
 	return(
-		<Register/>
+		<Router>
+			<Nav />
+			<Switch>
+
+				<Route path='/register' component={Register} />
+			</Switch>
+		</Router>
 	)
 }
---
+++ src/frontend/src/components/nav/Nav.js
@@ -0,0 +1,11 @@
+import React from "react"
+import './nav.css'
+
+export default function App(){
+	
+	return(
+		<div id='nav'>
+			<a href="/admin">admin</a>
+		</div>
+	)
+}
