552a69bffcb946f87e140fef5b6b44b8699e679f
owenmoogk <owenmoogk@gmail.com>
2021-05-04 16:40:47 -0400

    formatting

--- src/frontend/src/components/users/Login.js
+++ src/frontend/src/components/users/Login.js
@@ -2,45 +2,45 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 export default class Login extends React.Component {
-  state = {
-    username: '',
-    password: ''
-  };
+	state = {
+		username: '',
+		password: ''
+	};
 
-  handle_change = e => {
-    const name = e.target.name;
-    const value = e.target.value;
-    this.setState(prevstate => {
-      const newState = { ...prevstate };
-      newState[name] = value;
-      return newState;
-    });
-  };
+	handle_change = e => {
+		const name = e.target.name;
+		const value = e.target.value;
+		this.setState(prevstate => {
+			const newState = { ...prevstate };
+			newState[name] = value;
+			return newState;
+		});
+	};
 
-  render() {
-    return (
-      <form onSubmit={e => this.props.handle_login(e, this.state)}>
-        <h4>Log In</h4>
-        <label htmlFor="username">Username</label>
-        <input
-          type="text"
-          name="username"
-          value={this.state.username}
-          onChange={this.handle_change}
-        />
-        <label htmlFor="password">Password</label>
-        <input
-          type="password"
-          name="password"
-          value={this.state.password}
-          onChange={this.handle_change}
-        />
-        <input type="submit" />
-      </form>
-    );
-  }
+	render() {
+		return (
+			<form onSubmit={e => this.props.handle_login(e, this.state)}>
+				<h4>Log In</h4>
+				<label htmlFor="username">Username</label>
+				<input
+					type="text"
+					name="username"
+					value={this.state.username}
+					onChange={this.handle_change}
+				/>
+				<label htmlFor="password">Password</label>
+				<input
+					type="password"
+					name="password"
+					value={this.state.password}
+					onChange={this.handle_change}
+				/>
+				<input type="submit" />
+			</form>
+		);
+	}
 }
 
 Login.propTypes = {
-  handle_login: PropTypes.func.isRequired
+	handle_login: PropTypes.func.isRequired
 };
