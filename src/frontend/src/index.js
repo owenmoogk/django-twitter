6b9e5dca0bba43fee022afe0a862880cd2968bd8
owenmoogk <owenmoogk@gmail.com>
2021-05-07 19:46:39 -0400

    added some styling

--- src/frontend/src/index.js
+++ src/frontend/src/index.js
@@ -1,6 +1,7 @@
 import App from './components/App'
 import React from "react"
 import {render} from 'react-dom'
+import './index.css'
 
 const appDiv = document.getElementById('app')
 render(<App/>, appDiv)
