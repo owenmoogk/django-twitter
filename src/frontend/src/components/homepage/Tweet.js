58a09340356b4008f54d6efbb9b86ad8353b5ca4
owenmoogk <owenmoogk@gmail.com>
2021-05-12 12:25:29 -0400

    added images to tweets

--- src/frontend/src/components/homepage/Tweet.js
+++ src/frontend/src/components/homepage/Tweet.js
@@ -1,12 +1,19 @@
-import React, { useState } from "react"
+import React from "react"
 
 export default function Tweet(props){
 
 	return(
 		<a href={"/tweet/"+props.data.id} className="tweetLink">
 			<div className='tweet'>
-				<p><a href={'/user/' + props.data.user}><span className='username'>@{props.data.user}</span></a> • <span className='time'>{props.data.time}</span></p>
-				<p id='tweetContent'>{props.data.content}</p>
+				<div className='tweetImage'>
+					<div className='imageWrapper'>
+						<img src={'/media/'+props.data.user+".jpg"} onError={(e) => {e.target.style.display = 'none'}}/>
+					</div>
+				</div>
+				<div className='tweetText'>
+					<p><a href={'/user/' + props.data.user}><span className='username'>@{props.data.user}</span></a> • <span className='time'>{props.data.time}</span></p>
+					<p id='tweetContent'>{props.data.content}</p>
+				</div>
 			</div>
 		</a>
 	)
